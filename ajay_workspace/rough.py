e1c59fb6150f7bcdd082eae99e332a80445e043c
ajay <ajay>
2024-06-07 17:50:38 +0530

    commit map_reduce_filter

--- ajay_workspace/rough.py
+++ ajay_workspace/rough.py
@@ -1,1 +1,9 @@
-    return [string for string, length in map(lambda s: (s, len(s)), strings) if length > threshold]
+from functools import reduce
+
+def cumulative_sum(numbers):
+    return reduce(lambda acc, num: acc + [acc[-1] + num], numbers[1:], [numbers[0]])
+
+# Example usage:
+numbers = [1, 2, 3, 4, 5]
+cumulative_result = cumulative_sum(numbers)
+print(cumulative_result)
