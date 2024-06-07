42a9e4a8f0f25bb35035c7db8ef78d167b4b166f
_Prachi_Kumbhkar_ <prkumbhkar@bestpeers.com>
2024-06-07 12:52:55 +0530

    HigherOrderFun folder has been added

---
+++ prachi_workspace/HigherOrderFun/map.py
@@ -0,0 +1,12 @@
+# Write a  Python program to triple all numbers in a given list of integers. Use Python map
+
+L = [2,3,4,6]
+
+def triple(x):
+  return x*3
+
+result = map(triple, L)
+print(result)  # here it create a map object and map provides an iterator
+
+print(list(result))
+
