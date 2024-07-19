b405b458b7ca312ed6a26946cae959700a35eee7
ajay <ajay>
2024-07-19 18:57:42 +0530

    added register and login functionality

--- ajay_workspace/drf/image_uploader/imageapi/urls.py
+++ ajay_workspace/drf/image_uploader/imageapi/urls.py
@@ -3,6 +3,7 @@ from .views import RegisterUser, AuthenticateView, ImageModelView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("register/", RegisterUser.as_view()),
     path("login/", AuthenticateView.as_view()),
