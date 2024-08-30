024cebdffdd168c8b5d392f4d4cab4163a1d7ac0
ajay <ajay196.as@gmail.com>
2024-08-30 18:44:34 +0530

    added password reset functionality

--- ajay_workspace/django/image_uploader/image_uploader/wsgi.py
+++ ajay_workspace/django/image_uploader/image_uploader/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for image_uploader project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "image_uploader.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_uploader.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    sys.exit(1)
